631c4040fcfe0a70c42a4a570a70de8adabd7740
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-05 12:01:30 +0500

    map view model setup

--- app/src/main/java/xyz/teamgravity/parkingspotsaver/presentation/viewmodel/MapViewModel.kt
+++ app/src/main/java/xyz/teamgravity/parkingspotsaver/presentation/viewmodel/MapViewModel.kt
@@ -1,9 +1,17 @@
 package xyz.teamgravity.parkingspotsaver.presentation.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.MapProperties
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import xyz.teamgravity.parkingspotsaver.core.MapStyle
 import xyz.teamgravity.parkingspotsaver.data.model.ParkingSpotModel
 import xyz.teamgravity.parkingspotsaver.data.repository.ParkingSpotRepository
 import javax.inject.Inject
@@ -13,6 +21,47 @@ class MapViewModel @Inject constructor(
     private val repository: ParkingSpotRepository
 ) : ViewModel() {
 
+    var state by mutableStateOf(MapState())
+
+    init {
+        viewModelScope.launch {
+            repository.getAllParkingSpot().collectLatest { spots ->
+                state = state.copy(
+                    parkingSpots = spots
+                )
+            }
+        }
+    }
+
+    fun onEvent(event: MapEvent) {
+        when (event) {
+            MapEvent.ToggleFalloutMap -> {
+                state = state.copy(
+                    properties = state.properties.copy(
+                        mapStyleOptions = if (state.falloutMap) null else MapStyleOptions(MapStyle.FALLOUT)
+                    ),
+                    falloutMap = !state.falloutMap
+                )
+            }
+
+            is MapEvent.MapLongClick -> {
+                viewModelScope.launch {
+                    repository.insertParkingSpotSync(
+                        ParkingSpotModel(
+                            latitude = event.latLng.latitude,
+                            longitude = event.latLng.longitude
+                        )
+                    )
+                }
+            }
+
+            is MapEvent.InfoWindowLongClick -> {
+                viewModelScope.launch {
+                    repository.deleteParkingSpotSync(event.spot)
+                }
+            }
+        }
+    }
 
     sealed class MapEvent {
         object ToggleFalloutMap : MapEvent()
