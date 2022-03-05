fae047197b35a65c79f979ca1f17cf8fd5b9ba5c
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-05 11:36:43 +0500

    parking spot dao

---
+++ app/src/main/java/xyz/teamgravity/parkingspotsaver/data/local/ParkingSpotConst.kt
@@ -0,0 +1,20 @@
+package xyz.teamgravity.parkingspotsaver.data.local
+
+object ParkingSpotConst {
+
+    /**
+     * Database name
+     */
+    const val NAME = "parking_spot_database"
+
+    /**
+     * Database version
+     */
+    const val VERSION = 1
+
+    /**
+     * Table Parking Spot
+     * Model class -> [xyz.teamgravity.parkingspotsaver.data.model.ParkingSpotModel]
+     */
+    const val TABLE_PARKING_SPOT = "table_parking_spot"
+}
